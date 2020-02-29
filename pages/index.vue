3e98d976558a0be41ea66a069a16b825e460fe41
liuliang01 <liuliang01@sogou-inc.com>
2020-02-29 23:25:02 +0800

    first

---
+++ pages/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        nuxt-express
+      </h1>
+      <h2 class="subtitle">
+        My stylish Nuxt.js project
+      </h2>
+      <el-button type="primary" @click="click">主要按钮</el-button>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  methods: {
+    click() {
+      window.alert(11221)
+      window.console.log(3333)
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
